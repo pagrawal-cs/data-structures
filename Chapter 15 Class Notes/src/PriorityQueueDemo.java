c8f16c2859659d0f8ffd7006e66f6aa6b3e2e8e2
Priya Agrawal <pagrawal@stu.naperville203.org>
2024-09-17 14:09:35 -0500

    Stack, Queue, & PriorityQueue

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,24 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        // Create a to-do list
+        // The WorkOrder class has a priority and description
+        Queue<WorkOrder> toDo = new PriorityQueue<>();
+
+        // Lower priority is more important
+        toDo.add(new WorkOrder(3, "Water Plants"));
+        toDo.add(new WorkOrder(2, "Make Dinner"));
+        toDo.add(new WorkOrder(1, "Conquer World"));
+        toDo.add(new WorkOrder(9, "Play Videogames"));
+        toDo.add(new WorkOrder(1, "Study for the Ch. 15 Test"));
+
+        // Objects are NOT stored in priority order
+        System.out.println(toDo);
+        
+        // Objects will BE removed in priority order
+        while (toDo.size() > 0)
+        {
+            System.out.println(toDo.remove());
+        }
     }
 }
--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,28 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        // Create a print queue of strings (using a linked list)
+        Queue<String> jobs = new LinkedList<>();
+
+        // Add some print jobs
+        jobs.add("Joe: Quarter 2 Expense Report");
+        jobs.add("Cathy: Top Secret Document");
+
+        System.out.println("Printing: " + jobs.remove());
+
+        // Add some more jobs
+        jobs.add("Cathy: Really Top Secret Document");
+        jobs.add("Joe: Grocery List");
+        jobs.add("Cathy: Can I Get Fired For This?");
+
+        System.out.println("Printing: " + jobs.remove());
+
+        jobs.add("Boss: Cathy Termination Letter");
+
+        // Print the rest of the jobs in the queue
+        while (jobs.size() > 0)
+        {
+            System.out.println("Printing: " + jobs.remove()); 
+        }
     }
 }

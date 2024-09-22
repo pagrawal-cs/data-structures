ec25adf7e9f3054e607f2998385f56a21fd02ddf
pagrawal-cs <142527084+pagrawal-cs@users.noreply.github.com>
2024-09-22 18:11:31 -0500

    Update HTMLChecker.java
    Checked if the tags are properly nested: created a stack, if & else if structures to check for errors, and printed the error/success statement.

--- Chapter 15 Activities/HTMLChecker/src/HTMLChecker.java
+++ Chapter 15 Activities/HTMLChecker/src/HTMLChecker.java
@@ -17,18 +17,64 @@ public class HTMLChecker
 {
     public static void main(String[] args)
     {
-        String filename = "src/TagSample1.html";
-
+        String filename = "Chapter 15 Activities//HTMLChecker//src/TagSample3.html";
+        
         try (Scanner in = new Scanner(new File(filename)))
         {
             // Your code goes here
-            . . .
+            Stack<String> HTML = new Stack<>();
+            boolean NestedProperly = true;
 
+            while (in.hasNext())
+            {
+                String tag = in.next();
 
+                // Adds the opening HTML tags to the stack
+                if ((tag.startsWith("<")) && !(tag.startsWith("</")))
+                {
+                    // Checks if tags are opened more than once without being closed
+                    if (HTML.contains(tag))
+                    {
+                        System.out.println("\nThe tag " + tag + " cannot be opened twice before closing it first.\n");
+                        NestedProperly = false;
+                        break;
+                    }
+                    HTML.push(tag);
+                    //System.out.println(tag);
+                }
+                // Checks if tags are being closed in the proper order
+                else if (tag.startsWith("</"))
+                {
+                    // Checks if uncreated tags are being closed
+                    if (HTML.isEmpty())
+                    {
+                        System.out.println("\nThe tag " + tag + " hasn't been created yet, so it canot be closed.");
+                        NestedProperly = false;
+                        break;
+                    }
+                    // Checks if the correct tag is being closed
+                    else
+                    {
+                        String last = HTML.pop();
+                        String expectedTag = "<" + tag.substring(2);
+
+                        if(!(last.equals(expectedTag)))
+                        {
+                            System.out.println("\nThe tag " + tag + " hasn't been properly nested - it has been closed before another tag should be closed.\n");
+                            NestedProperly = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // Prints if everything runs smoothly
+            if (NestedProperly == true)
+                System.out.println("\nAll the HTML tags are properly nested! :)\n");
         } catch (FileNotFoundException e)
         {
             System.out.println("Cannot open: " + filename);
         }
-
+        
     }
 }

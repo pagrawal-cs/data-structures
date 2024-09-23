e45803975b2bb64527ec2273ab85e57faaa7b4e1
Priya Agrawal <pagrawal@stu.naperville203.org>
2024-09-23 13:49:05 -0500

    SentenceReverser Pair Program completed

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -14,50 +14,46 @@ public class SentenceReverser
     */
     public static String reverse(String sentence)
     {
-    	Scanner scanner = new Scanner(sentence);
-        String word ="";
-    	
+        Scanner scanner = new Scanner(sentence);
+        String word =" ", backwards =""; String temp = "";
+        
         // Complete this method. Use a Stack.
         Stack<String> reverser = new Stack<>();
 
-        for (int i = 0; i < sentence.length()-1; i++)
+        for (int i = 0; i < sentence.length(); i++)
         {
-            if (sentence.substring(i, i + 1).equals(",") ){
-                //word += sentence.substring(i, i + 1);
-                word = ", " + word;
-
-                reverser.push(word);
-                word = "";
-
-
-            }
-            else if (!(sentence.substring(i, i + 1).equals(" ")))
+            if (sentence.substring(i, i + 1).equals(" "))
             {
-                word += sentence.substring(i, i + 1);
-                reverser.push(word);
+              reverser.push(word.toLowerCase());
                 word = "";
 
             }
-            else if (!(sentence.substring(i, i + 1).equals(".")))
+            else if (sentence.substring(i, i + 1).equals("."))
             {
-                reverser.push(word);
+                
+                reverser.push(word.toLowerCase());
                 word = "";
+                for (int j = 0; j < reverser.size()+4; j++){
+                    temp = reverser.pop();
+                    if (j == 0){
+                        
+                        backwards += temp.substring(0,2).toUpperCase() + temp.substring(2);
+                    }
+                    else
+                       backwards += temp;
 
-                for (int j = 0; j < reverser.size(); j++){
-                    System.out.println(reverser.pop());
+                    if (j == reverser.size()+4)
+                        backwards += ".";
                 }
 
             }
+                word += sentence.substring(i, i + 1).toLowerCase();
             
             
         }
 
 
-
-
-
-
-        return "";
+        return backwards;
 
     }
 }

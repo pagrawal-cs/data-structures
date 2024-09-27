054950255658ce4de8cddbfa6e56c77d161cc3ab
Priya Agrawal <pagrawal@stu.naperville203.org>
2024-09-27 14:14:10 -0500

    Completed LinkedList lesson

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -75,7 +75,17 @@ public class LinkedList
         return new LinkedListIterator();
     }
 
+    public String toString() {
+        ListIterator listIterator = listIterator();
 
+        String allElements = "[";
+
+        while (listIterator.hasNext()) {
+            allElements += listIterator.next() + ", ";
+        }
+
+        return allElements+"]";
+    }
 
 
     //Class Node
@@ -84,24 +94,47 @@ public class LinkedList
         public Node next;
         public Object data;
     }
-
+    
 
     class LinkedListIterator implements ListIterator
     {
       //private data
-
+        private Node position;
+        private Node previous; // necessary for removing elements
+        private boolean isAfterNext;
 
         /**
             Constructs an iterator that points to the front
             of the linked list.
         */
-
+        public LinkedListIterator() {
+            position = null;
+            previous = null;
+            isAfterNext = false;
+        }
 
         /**
             Moves the iterator past the next element.
             @return the traversed element
         */
+        public Object next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
 
+            previous = position;
+
+            if (position == null) {
+                position = first;
+            }
+            else {
+                position = position.next;
+            }
+
+            isAfterNext = true;
+
+            return position.data;
+        }
 
 
 
@@ -110,27 +143,60 @@ public class LinkedList
             Tests if there is an element after the iterator position.
             @return true if there is an element after the iterator position
         */
+        public boolean hasNext() {
+            // Check if the list is empty
+            if (position == null) { // The iterator hasn't moved
+                return first != null;
+            }
 
+            // The iterator has moved
+            return position.next != null;
+        }
 
         /**
             Adds an element before the iterator position
             and moves the iterator past the inserted element.
             @param element the element to add
         */
+        public void add(Object element) {
+            // Check if the iterator is at the beginning
+            if (position == null) {
+                addFirst(element);
+                position = first;
+            }
+            else {
+                Node newNode = new Node();
+                newNode.data = element;
+                newNode.next = position.next;
+                // Set the next element of the CURRENT position to point to our new node
+                position.next = newNode;
+                position = newNode;
+            }
 
-
-
-
-
+            isAfterNext = false;
+        }
 
         /**
             Removes the last traversed element. This method may
             only be called after a call to the next() method.
         */
+        public void remove() {
+            if (!isAfterNext) {
+                throw new IllegalStateException();
+            }
 
+            // Check if the iterator is at the beginning
+            if (position == first) {
+                removeFirst();
+                position = null;
+            }
+            else {
+                previous.next = position.next;
+                position = previous;
+            }
 
-
-
+            isAfterNext = false;
+        }
 
 
 
@@ -138,7 +204,13 @@ public class LinkedList
             Sets the last traversed element to a different value.
             @param element the element to set
         */
+        public void set(Object element) {
+            if (!isAfterNext) {
+                throw new IllegalStateException();
+            }
 
+            position.data = element;
+        }
 
 
 
--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,5 +5,13 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        LinkedList students = new LinkedList();
+
+        students.addFirst("Arthur");
+        students.addFirst("Bijoux");
+        students.addFirst("Jinan");
+        students.addFirst("Andy");
+        
+        System.out.println(students);
     }
 }
